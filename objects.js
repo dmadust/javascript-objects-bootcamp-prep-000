b63813b3d00986ce7e1a5e6ca31bb5689547f170
dmadust@gmail.com <dmadust@gmail.com>
2018-04-20 23:04:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 function updatePlaylist(playlist, artist, title) {
-  playlist.artist = title;
+  playlist[artist] = title;
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist;
+  delete playlist[artist]
   return playlist;
 }
 
