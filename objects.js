7a711d98ed73c1e2ebe76979eded8f9f26cc322a
dmadust@gmail.com <dmadust@gmail.com>
2018-04-20 23:02:57 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,15 @@
+playlist = { 
+  aviici : "Wake Me Up",
+  ladyGaga : "Born This Way",
+  beyonce : "Crazy in Love"
+};
+
+function updatePlaylist(playlist, artist, title) {
+  playlist.artist = title;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist;
+  return playlist;
+}
